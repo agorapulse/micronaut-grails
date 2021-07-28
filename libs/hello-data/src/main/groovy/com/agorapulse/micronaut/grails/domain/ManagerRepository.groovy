168942264f83dde3fb6324c33c521a45c320fdbc
Vladimir Orany <vladimir@orany.cz>
2021-07-28 15:04:47 +0200

    Revert "added forgotten test fixtures"
    This reverts commit 2ee3c5edc6215bf678c5319b28beb99357eb36bd.

--- libs/hello-data/src/main/groovy/com/agorapulse/micronaut/grails/domain/ManagerRepository.groovy
+++
@@ -1,9 +0,0 @@
-package com.agorapulse.micronaut.grails.domain
-
-import io.micronaut.data.annotation.Repository
-import io.micronaut.data.repository.CrudRepository
-
-@Repository
-interface ManagerRepository extends CrudRepository<Manager, Long> {
-
-}
